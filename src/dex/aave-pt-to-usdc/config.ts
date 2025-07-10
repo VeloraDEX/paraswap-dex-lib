6d81031c8d57321ca1ad97ce8125166c4a942119
0xAurelou <0xaurelou@gmail.com>
2025-07-10 12:54:28 +0200

    feat: init archi

---
+++ src/dex/aave-pt-to-usdc/config.ts
@@ -0,0 +1,9 @@
+import { DexParams } from './types';
+import { DexConfigMap } from '../../types';
+import { Network, SwapSide } from '../../constants';
+
+export const AavePtToUsdcConfig: DexConfigMap<DexParams> = {
+  AavePtToUsdc: {
+    // TODO: complete me!
+  },
+};
