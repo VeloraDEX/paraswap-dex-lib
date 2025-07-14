4ca087c633c383531c13c17f5c97fd71d317ac3b
Ivan Nedokhodiuk <nedokhodiukivan@gmail.com>
2025-07-14 11:33:25 +0300

    Merge branch 'feat/pangolinv3'

--- src/dex/uniswap-v3/config.ts
+++ src/dex/uniswap-v3/config.ts
@@ -10,9 +10,13 @@ import { decodeStateMultiCallResultWithRelativeBitmaps as decodeStateMultiCallRe
 import { RamsesV2EventPool } from './forks/ramses-v2/ramses-v2-pool';
 import { VelodromeSlipstreamEventPool } from './forks/velodrome-slipstream/velodrome-slipstream-pool';
 import { VelodromeSlipstreamFactory } from './forks/velodrome-slipstream/velodrome-slipstream-factory';
+import PangolinV3StateMulticallABI from '../../abi/pangolin-v3/PangolinV3StateMulticall.abi.json';
+import { decodeStateMultiCallResultWithRelativeBitmaps as decodeStateMultiCallResultWithRelativeBitmapsForPangolin } from './forks/pangolin-v3/utils';
+import { PangolinV3EventPool } from './forks/pangolin-v3/pangolin-v3-pool';
 
 const SUPPORTED_FEES = [10000n, 3000n, 500n, 100n];
 const RAMSES_FORKS_FEES = [...SUPPORTED_FEES, 50n, 250n];
+const PANGOLIN_SUPPORTED_FEES = [8000n, 2500n, 500n, 100n];
 
 // Pools that will be initialized on app startup
 // They are added for testing
@@ -317,7 +321,7 @@ export const UniswapV3Config: DexConfigMap<DexParams> = {
   SpookySwapV3: {
     [Network.SONIC]: {
       factory: '0x3D91B700252e0E3eE7805d12e048a988Ab69C8ad',
-      quoter: '0x593856bbfd6Aaf0b714277c0BF06307900d1Aa68',
+      quoter: '0x3F2026Cae76b987C4002e62B9dF70988b4388234',
       router: '0x0C2BC01d435CfEb2DC6Ad7cEC0E473e2DBaBdd87',
       routerType: UniswapV3Router.SwapRouter02,
       supportedFees: SUPPORTED_FEES,
@@ -469,6 +473,25 @@ export const UniswapV3Config: DexConfigMap<DexParams> = {
       initRetryFrequency: 10,
       initHash: '0xeC8E5342B19977B4eF8892e02D8DAEcfa1315831', // pool implementation address from factory contract is used instead of initHash here
       subgraphURL: 'GENunSHWLBXm59mBSgPzQ8metBEp9YDfdqwFr91Av1UM',
+    },
+  },
+  PangolinV3: {
+    [Network.AVALANCHE]: {
+      factory: '0x1128F23D0bc0A8396E9FBC3c0c68f5EA228B8256',
+      quoter: '0xA86522CCc412dBC4FA10991900FE46De95983822',
+      router: '0x5485A0751a249225D3bA2f6f296551507e22547f',
+      supportedFees: PANGOLIN_SUPPORTED_FEES,
+      stateMulticall: '0x30F6B9b6485ff0B67E881f5ac80D3F1c70A4B23d',
+      stateMultiCallAbi: PangolinV3StateMulticallABI as AbiItem[],
+      eventPoolImplementation: PangolinV3EventPool,
+      decodeStateMultiCallResultWithRelativeBitmaps:
+        decodeStateMultiCallResultWithRelativeBitmapsForPangolin,
+      uniswapMulticall: '0x7d115C1fb6152C5Aed1750183Ae59107160694a2',
+      chunksCount: 10,
+      initRetryFrequency: 10,
+      initHash:
+        '0xa9bb1321d78097b23af97a9c07d5ec13e1adc404334585171c54ecccb5ad93b2',
+      subgraphURL: 'EMnAvnfc1fwGSU6ToqYJCeEkXmSgmDmhwtyaha1tM5oi',
     },
   },
   Wagmi: {
