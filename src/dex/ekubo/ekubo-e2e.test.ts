ccbb94418160cbc0c8bffb4c54add4fe888c89f7
Ivan Nedokhodiuk <nedokhodiukivan@gmail.com>
2025-08-21 09:32:51 +0300

    Merge branch 'ekubo-mev-resist'

--- src/dex/ekubo/ekubo-e2e.test.ts
+++ src/dex/ekubo/ekubo-e2e.test.ts
@@ -3,52 +3,37 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { StaticJsonRpcProvider } from '@ethersproject/providers';
-import {
-  Holders,
-  NativeTokenSymbols,
-  Tokens,
-} from '../../../tests/constants-e2e';
+import { Holders, Tokens } from '../../../tests/constants-e2e';
 import { testE2E } from '../../../tests/utils-e2e';
 import { generateConfig } from '../../config';
 import { ContractMethod, Network, SwapSide } from '../../constants';
 import { PoolConfig, PoolKey } from './pools/utils';
+import { BI_POWS } from '../../bigint-constants';
+import { DEX_KEY, EKUBO_CONFIG } from './config';
 
-function testForNetwork(
-  network: Network,
-  dexKey: string,
-  nativeTokenAmount: string,
-) {
-  const provider = new StaticJsonRpcProvider(
-    generateConfig(network).privateHttpProvider,
-    network,
-  );
+describe('Mainnet', () => {
+  const network = Network.MAINNET;
   const tokens = Tokens[network];
-  const holders = Holders[network];
-  const nativeTokenSymbol = NativeTokenSymbols[network];
-
-  const sideToContractMethods = new Map([
-    [SwapSide.SELL, [ContractMethod.swapExactAmountIn]],
-    [SwapSide.BUY, [ContractMethod.swapExactAmountOut]],
-  ]);
+  const config = EKUBO_CONFIG[DEX_KEY][network];
 
   const tokensToTest = [
     {
       pair: [
         {
-          symbol: 'WBTC',
-          amount: '100000000',
+          symbol: 'USDC',
+          amount: '10000000',
         },
         {
-          symbol: 'cbBTC',
-          amount: '100000000',
+          symbol: 'USDT',
+          amount: '10000000',
         },
       ],
     },
     {
       pair: [
         {
-          symbol: nativeTokenSymbol,
-          amount: nativeTokenAmount,
+          symbol: 'ETH',
+          amount: '1000000000000000',
         },
         {
           symbol: 'USDC',
@@ -71,75 +56,80 @@ function testForNetwork(
     {
       pair: [
         {
-          symbol: 'USDC',
-          amount: '10000000',
+          symbol: 'EKUBO',
+          amount: BI_POWS[18],
         },
         {
-          symbol: 'USDT',
-          amount: '10000000',
+          symbol: 'ebUSD',
+          amount: BI_POWS[19],
         },
+      ],
+      limitPools: [
+        new PoolKey(
+          BigInt(tokens['EKUBO'].address),
+          BigInt(tokens['ebUSD'].address),
+          new PoolConfig(500, 1844674407370955n, BigInt(config.mevResist)),
+        ).string_id,
       ],
     },
   ];
 
-  describe(`${network}`, () => {
-    sideToContractMethods.forEach((contractMethods, side) =>
-      describe(`${side}`, () => {
-        contractMethods.forEach((contractMethod: ContractMethod) => {
-          describe(`${contractMethod}`, () => {
-            function test(
-              srcTokenSymbol: string,
-              destTokenSymbol: string,
-              amount: string,
-              side: SwapSide,
-              poolIdentifiers?: string[],
-            ) {
-              return testE2E(
-                tokens[srcTokenSymbol],
-                tokens[destTokenSymbol],
-                holders[srcTokenSymbol],
-                amount,
-                side,
-                dexKey,
-                contractMethod,
-                network,
-                provider,
-                poolIdentifiers && { [dexKey]: poolIdentifiers },
-              );
-            }
+  const provider = new StaticJsonRpcProvider(
+    generateConfig(network).privateHttpProvider,
+    network,
+  );
+  const holders = Holders[network];
 
-            tokensToTest.forEach(({ pair: [tokenA, tokenB], limitPools }) => {
-              it(`${tokenA.symbol} -> ${tokenB.symbol}`, () =>
-                test(
-                  tokenA.symbol,
-                  tokenB.symbol,
-                  side === SwapSide.SELL ? tokenA.amount : tokenB.amount,
-                  side,
-                  limitPools,
-                ));
-              it(`${tokenB.symbol} -> ${tokenA.symbol}`, () =>
-                test(
-                  tokenB.symbol,
-                  tokenA.symbol,
-                  side === SwapSide.SELL ? tokenB.amount : tokenA.amount,
-                  side,
-                  limitPools,
-                ));
-            });
+  const sideToContractMethods = new Map([
+    [SwapSide.SELL, [ContractMethod.swapExactAmountIn]],
+    [SwapSide.BUY, [ContractMethod.swapExactAmountOut]],
+  ]);
+
+  sideToContractMethods.forEach((contractMethods, side) =>
+    describe(`${side}`, () => {
+      contractMethods.forEach((contractMethod: ContractMethod) => {
+        describe(`${contractMethod}`, () => {
+          function test(
+            srcTokenSymbol: string,
+            destTokenSymbol: string,
+            amount: string,
+            side: SwapSide,
+            poolIdentifiers?: string[],
+          ) {
+            return testE2E(
+              tokens[srcTokenSymbol],
+              tokens[destTokenSymbol],
+              holders[srcTokenSymbol],
+              amount,
+              side,
+              DEX_KEY,
+              contractMethod,
+              network,
+              provider,
+              poolIdentifiers && { [DEX_KEY]: poolIdentifiers },
+            );
+          }
+
+          tokensToTest.forEach(({ pair: [tokenA, tokenB], limitPools }) => {
+            it(`${tokenA.symbol} -> ${tokenB.symbol}`, () =>
+              test(
+                tokenA.symbol,
+                tokenB.symbol,
+                String(side === SwapSide.SELL ? tokenA.amount : tokenB.amount),
+                side,
+                limitPools,
+              ));
+            it(`${tokenB.symbol} -> ${tokenA.symbol}`, () =>
+              test(
+                tokenB.symbol,
+                tokenA.symbol,
+                String(side === SwapSide.SELL ? tokenB.amount : tokenA.amount),
+                side,
+                limitPools,
+              ));
           });
         });
-      }),
-    );
-  });
-}
-
-describe('Ekubo E2E', () => {
-  const dexKey = 'Ekubo';
-
-  describe('Mainnet', () => {
-    const network = Network.MAINNET;
-    const nativeTokenAmount = '1000000000000000';
-
-    testForNetwork(network, dexKey, nativeTokenAmount);
-  });
+      });
+    }),
+  );
 });
