ab8138fce40362d472153d99233f1a8baf393a60
Ivan Nedokhodiuk <nedokhodiukivan@gmail.com>
2026-02-03 10:32:45 +0200

    Revert "fix(pricing): adjust BUY side amount"
    This reverts commit 541f2a5b4b4d5864901ece7b75f9ec49ea9a857c.

--- src/dex/fluid-dex/fluid-dex.ts
+++ src/dex/fluid-dex/fluid-dex.ts
@@ -990,10 +990,8 @@ export class FluidDex extends SimpleExchange implements IDex<FluidDexData> {
     centerPrice: bigint,
     syncTime: number,
   ): bigint {
-    const amountOutWithBuffer = (amountOut * 1000001n) / 1000000n;
-
     const amountOutAdjusted =
-      (amountOutWithBuffer * BigInt(10 ** 12)) / BigInt(10 ** outDecimals);
+      (amountOut * BigInt(10 ** 12)) / BigInt(10 ** outDecimals);
 
     const amountIn = this.swapOutAdjusted(
       swap0to1,
