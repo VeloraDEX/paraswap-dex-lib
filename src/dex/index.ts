82dbf2ba61ce4bd69264effb2960efa2090b0acf
Ivan Nedokhodiuk <nedokhodiukivan@gmail.com>
2026-02-11 02:07:11 +0800

    feat(integration): include `Clear` in Dexes list

--- src/dex/index.ts
+++ src/dex/index.ts
@@ -98,7 +98,7 @@ import { Blackhole } from './solidly/forks-override/blackhole';
 import { BlackholeCL } from './algebra-integral/forks/blackhole-cl';
 import { dETH } from './deth/dETH';
 import { Cap } from './cap/cap';
-import { Clear } from './clear/clear'; // TODO: Enable after configuration is complete
+import { Clear } from './clear/clear';
 
 const LegacyDexes = [
   CurveV2,
@@ -190,7 +190,7 @@ const Dexes = [
   Blackhole,
   BlackholeCL,
   Cap,
-  // Clear, // TODO: Uncomment after configuration is complete and tests pass
+  Clear,
 ];
 
 export type LegacyDexConstructor = new (dexHelper: IDexHelper) => IDexTxBuilder<
