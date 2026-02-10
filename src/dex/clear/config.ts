5f14e4074adbcdfb4d6a48b1410d5066afc65f74
Ivan Nedokhodiuk <nedokhodiukivan@gmail.com>
2026-02-11 02:02:50 +0800

    chore(dexKey): use uppercase format

--- src/dex/clear/config.ts
+++ src/dex/clear/config.ts
@@ -3,7 +3,7 @@ import { DexConfigMap } from '../../types';
 import { Network } from '../../constants';
 
 export const ClearConfig: DexConfigMap<DexParams> = {
-  clear: {
+  Clear: {
     [Network.MAINNET]: {
       factoryAddress: '0x8bF266ED803e474AE7Bf09ADB5ba2566c489223d',
       swapAddress: '0xeb5AD3D93E59eFcbC6934caD2B48EB33BAf29745',
