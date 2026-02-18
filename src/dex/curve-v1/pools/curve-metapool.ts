ee9de7c42e623ab7d33b2a42ca39c8bc17848147
Ivan Nedokhodiuk <nedokhodiukivan@gmail.com>
2026-02-18 16:56:17 +0200

    fix(log): use correct abi for handling

--- src/dex/curve-v1/pools/curve-metapool.ts
+++ src/dex/curve-v1/pools/curve-metapool.ts
@@ -12,7 +12,7 @@ import { BlockHeader } from 'web3-eth';
 
 import { BN_0, BN_600, BN_POWS } from '../../../bignumber-constants';
 import { IDexHelper } from '../../../dex-helper';
-import { erc20Iface } from '../../../lib/utils-interfaces';
+import { erc20Iface } from '../../../lib/tokens/utils';
 import { bigNumberify, stringify } from '../../../utils';
 import { getManyPoolStates } from './getstate-multicall';
 
