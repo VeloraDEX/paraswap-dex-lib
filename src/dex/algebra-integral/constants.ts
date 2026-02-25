e4fd64542248ba5d6ef61a6c1870c3bf0883ca68
Ivan Work <your-work@email>
2026-02-25 23:22:18 +0200

    chore(vars): remove redundant

--- src/dex/algebra-integral/constants.ts
+++ src/dex/algebra-integral/constants.ts
@@ -16,6 +16,3 @@ export const TICK_BITMAP_TO_USE = 400n;
 export const TICK_BITMAP_BUFFER = 800n;
 export const TICK_BITMAP_TO_USE_BY_CHAIN: Record<number, bigint> = {};
 export const TICK_BITMAP_BUFFER_BY_CHAIN: Record<number, bigint> = {};
-export const MAX_PRICING_COMPUTATION_STEPS_ALLOWED = 4096;
-export const MAX_BATCH_SIZE = 100;
-export const MAX_NUMBER_OF_BATCH_REQUEST_HALVING = 4;
