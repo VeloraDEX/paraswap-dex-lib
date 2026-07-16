ac85b3cdd56237072b283bd1743d304d3dec6bc5
Daniel Toth <daniel@poolparty.market>
2026-07-16 15:30:21 +0200

    feat(flowstate): add FlowState C1 pools as a liquidity source

--- src/dex/index.ts
+++ src/dex/index.ts
@@ -107,6 +107,7 @@ import { Cap } from './cap/cap';
 import { PancakeSwapInfinity } from './pancakeswap-infinity/pancakeswap-infinity';
 import { Metric } from './metric/metric';
 import { Tessera } from './tessera/tessera';
+import { FlowState } from './flowstate/flowstate';
 
 const LegacyDexes = [
   CurveV2,
@@ -201,6 +202,7 @@ const Dexes = [
   Blackhole,
   BlackholeCL,
   Cap,
+  FlowState,
 ];
 
 export type LegacyDexConstructor = new (dexHelper: IDexHelper) => IDexTxBuilder<
